1f04675f9955b9f3eec9e7f469781980f1187f0a
CDA-Benseddik-Fethi <58114380+CDA-Benseddik-Fethi@users.noreply.github.com>
2020-06-16 10:09:29 +0200

    Ajout constante

--- src/com/cda/model/MaFenetre.java
+++ src/com/cda/model/MaFenetre.java
@@ -3,6 +3,8 @@ package com.cda.model;
 import javax.swing.*;
 
 public class MaFenetre extends JFrame {
+    public static final  String FOND_ECRAN = "null";
+
     public MaFenetre() {
         setSize(600, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);

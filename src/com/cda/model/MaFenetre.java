6a017015f9df90613539730f2bcb5bb134b061f7
CDA-Benseddik-Fethi <58114380+CDA-Benseddik-Fethi@users.noreply.github.com>
2020-06-16 09:43:54 +0200

    Definition taille fenetre de jeu

--- src/com/cda/model/MaFenetre.java
+++ src/com/cda/model/MaFenetre.java
@@ -3,4 +3,12 @@ package com.cda.model;
 import javax.swing.*;
 
 public class MaFenetre extends JFrame {
+    public MaFenetre() {
+        setSize(600, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Test");
+        setVisible(true);
+        setResizable(false);
+
+    }
 }
--- src/com/cda/program/Program.java
+++ src/com/cda/program/Program.java
@@ -1,4 +1,10 @@
 package com.cda.program;
 
+import com.cda.model.MaFenetre;
+
 public class Program {
+
+    public static void main(String[] args) {
+        new MaFenetre();
+    }
 }

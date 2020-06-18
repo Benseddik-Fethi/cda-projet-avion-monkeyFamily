37d8a880503407079c45bcb9cd073e1e9ad2a936
Antoine Auzoux <antoine.auzoux@gmail.com>
2020-06-18 12:22:30 +0200

    tir-avion / score

--- src/com/cda/model/Constantes.java
+++ src/com/cda/model/Constantes.java
@@ -1,7 +1,5 @@
 package com.cda.model;
 
-import com.cda.utils.Tools;
-
 import java.util.ArrayList;
 
 /**
@@ -9,110 +7,108 @@ import java.util.ArrayList;
  */
 public class Constantes {
 
-    /**
-     * Utilis� dans la classe Ecouteur Vaisseau dans le package com.cda.listener.
-     *
-     * @param <b>VITESSE</b> permet de r�gler la vitesse du vaisseau en modifiant la
-     * valeur.
-     */
-    public static final int VITESSE_VAISSEAU = 5;
+	/**
+	 * Utilis� dans la classe Ecouteur Vaisseau dans le package com.cda.listener.
+	 *
+	 * @param <b>VITESSE</b> permet de r�gler la vitesse du vaisseau en modifiant la
+	 *                       valeur.
+	 */
+	public static final int VITESSE_VAISSEAU = 5;
 
-    /**
-     * Utilis� dans la classe Chrono dans le package com.cda.model.
-     *
-     * @param <b>VITESSE_DEFILEMENT_FOND</b> permet de r�gler la vitesse de
-     * d�filement du fond en modifiant la
-     * valeur.
-     */
-    protected final static int VITESSE_DEFILEMENT_FOND = 5;
+	/**
+	 * Utilis� dans la classe Chrono dans le package com.cda.model.
+	 *
+	 * @param <b>VITESSE_DEFILEMENT_FOND</b> permet de r�gler la vitesse de
+	 *                                       d�filement du fond en modifiant la
+	 *                                       valeur.
+	 */
+	protected final static int VITESSE_DEFILEMENT_FOND = 5;
 
-    /**
-     * Utilis� dans la classe MaFenetre dans le package com.cda.listener.
-     *
-     * @param <b>FENETRE_WIDTH</b> permet de param�trer la largeur de la fen�tre de
-     * jeu.
-     */
-    protected final static int FENETRE_WIDTH = 600;
+	/**
+	 * Utilis� dans la classe MaFenetre dans le package com.cda.listener.
+	 *
+	 * @param <b>FENETRE_WIDTH</b> permet de param�trer la largeur de la fen�tre de
+	 *                             jeu.
+	 */
+	protected final static int FENETRE_WIDTH = 600;
 
-    /**
-     * Utilis� dans les classes MaFenetre, BackgroundWindow, MaFenetre dans le
-     * package com.cda.listener.
-     *
-     * @param <b>FENETRE_HEIGHT</b> permet de param�trer la hauteur de la fen�tre de
-     * jeu.
-     */
-    protected final static int FENETRE_HEIGHT = 800;
+	/**
+	 * Utilis� dans les classes MaFenetre, BackgroundWindow, MaFenetre dans le
+	 * package com.cda.listener.
+	 *
+	 * @param <b>FENETRE_HEIGHT</b> permet de param�trer la hauteur de la fen�tre de
+	 *                              jeu.
+	 */
+	protected final static int FENETRE_HEIGHT = 900;
 
-    /**
-     * Utilis� dans les classes BackgroundWindow, MaFenetre dans le package
-     * com.cda.listener.
-     *
-     * @param <b>IMAGE_HEIGHT</b> permet de param�trer la hauteur du background,
-     * utilis� pour le d�filement vertical.
-     */
-    protected final static int IMAGE_HEIGHT = FENETRE_HEIGHT / 2;
+	/**
+	 * Utilis� dans les classes BackgroundWindow, MaFenetre dans le package
+	 * com.cda.listener.
+	 *
+	 * @param <b>IMAGE_HEIGHT</b> permet de param�trer la hauteur du background,
+	 *                            utilis� pour le d�filement vertical.
+	 */
+	protected final static int IMAGE_HEIGHT = FENETRE_HEIGHT / 2;
 
-    /**
-     * @param <b>IMAGES</b>
-     */
-    // Utilis� dans les classes Vaisseau, MaFenetre dans le package
-    // com.cda.listener.
-    protected static final String AVION_TOP = "/images/avion-top.png";
+	/**
+	 * @param <b>IMAGES</b>
+	 */
+	// Utilis� dans les classes Vaisseau, MaFenetre dans le package
+	// com.cda.listener.
+	protected static final String AVION_TOP = "/images/avion-top.png";
 
-    // Utilis� dans la classe Vaisseau dans le package com.cda.listener.
-    protected static final String AVION_LEFT = "/images/avion-left.png";
+	// Utilis� dans la classe Vaisseau dans le package com.cda.listener.
+	protected static final String AVION_LEFT = "/images/avion-left.png";
 
-    // Utilis� dans la classe Vaisseau dans le package com.cda.listener.
-    protected static final String AVION_RIGHT = "/images/avion-right.png";
+	// Utilis� dans la classe Vaisseau dans le package com.cda.listener.
+	protected static final String AVION_RIGHT = "/images/avion-right.png";
 
-    // Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
-    protected static final String FOND = "/images/fond.jpg";
+	// Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
+	protected static final String FOND = "/images/fond.jpg";
 
-/*========Constante Avion========*/
-    public static final int HAUTEUR_AVION = 70;
-    public static final int LARGEUR_AVION = 70;
-    public static final int POSITIONX_DEPART_AVION =250;
-    public static final int POSITIONY_DEPART_AVION =650;
+	/* ========Constante Avion======== */
+	public static final int HAUTEUR_AVION = 100;
+	public static final int LARGEUR_AVION = 100;
+	public static final int POSITIONX_DEPART_AVION = 250;
+	public static final int POSITIONY_DEPART_AVION = 650;
 
-    /*-----------------Constante Missile---------*/
-    protected static final int LARGEUR_MISSILE = 50;
-    protected static final int HAUTEUR_MISSILE = 50;
-    protected static final int POSITION_DEPART = 0;
+	/*-----------------Constante Missile---------*/
+	protected static final int LARGEUR_MISSILE = 50;
+	protected static final int HAUTEUR_MISSILE = 50;
+	protected static final int POSITION_DEPART = 0;
 
+	/* ============Constante Missile Feu========== */
+	// Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
+	protected static final String MISSILE_FEU = "/images/missile-feu.png";
+	protected static final int VITESSE_CHUTE_FEU = 3;
 
-    /*============Constante Missile Feu==========*/
-    // Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
-    protected static final String MISSILE_FEU = "/images/missile-feu.png";
-    protected static final int VITESSE_CHUTE_FEU = 3;
+	/* ============Constante Missile Glace========== */
+	// Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
+	protected static final String MISSILE_GLACE = "/images/missile-glace.png";
+	protected static final int VITESSE_CHUTE_GLACE = 2;
 
-    /*============Constante Missile Glace==========*/
-    // Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
-    protected static final String MISSILE_GLACE = "/images/missile-glace.png";
-    protected static final int VITESSE_CHUTE_GLACE = 2;
+	/* ============Constante Missile Normal========== */
+	// Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
+	protected static final String MISSILE_NORMAL = "/images/missile-normal.png";
+	protected static final int VITESSE_CHUTE_NORMAL = 2;
 
-    /*============Constante Missile Normal==========*/
-    // Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
-    protected static final String MISSILE_NORMAL = "/images/missile-normal.png";
-    protected static final int VITESSE_CHUTE_NORMAL = 2;
+	/* ============Constante Missile ZigZag========== */
+	// Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
+	protected static final String MISSILE_ZIGZAG = "/images/missile-zigzag.png";
+	protected static final String MISSILE_ZIGZAG_DROITE = "/images/missile-zigzag-droite.png";
+	protected static final String MISSILE_ZIGZAG_GAUCHE = "/images/missile-zigzag-gauche.png";
+	protected static final int VITESSE_CHUTE_ZIGZAG = 1;
 
-    /*============Constante Missile ZigZag==========*/
-    // Utilis� dans la classe BackgroundWindow dans le package com.cda.listener.
-    protected static final String MISSILE_ZIGZAG = "/images/missile-zigzag.png";
-    protected static final String MISSILE_ZIGZAG_DROITE = "/images/missile-zigzag-droite.png";
-    protected static final String MISSILE_ZIGZAG_GAUCHE = "/images/missile-zigzag-gauche.png";
-    protected static final int VITESSE_CHUTE_ZIGZAG = 1;
+	/* =======Constante Tableau Missile======== */
+	protected static final int NOMBRE_MISSILE_INIT = 3;
+	protected static final ArrayList<Entite> MES_MISSILES = new ArrayList<>();
 
-    /*=======Constante Tableau Missile========*/
-    protected static final int NOMBRE_MISSILE_INIT = 3;
-    protected static final ArrayList<Entite> MES_MISSILES = new ArrayList<>();
-
-
-    /*==============Constante Missile Avion=====*/
-    public static final int HAUTEUR_MISSILE_AVION= 20;
-    public static final int LARGEUR_MISSILE_AVION= 60;
-    protected static final String IMAGE_MISSILE_AVION = "/images/missile.png";
-    public static final int DELTA_MISSILE = 6;
-    protected static final int NOMBRE_MISSILE_TIR_INIT = 10;
-    protected static final ArrayList<Entite> MES_TIR_MISSILE = new ArrayList<>();
+	/* ==============Constante Missile Avion===== */
+	public static final int HAUTEUR_MISSILE_AVION = 20;
+	public static final int LARGEUR_MISSILE_AVION = 60;
+	protected static final String IMAGE_MISSILE_AVION = "/images/tir-avion.png";
+	public static final int DELTA_MISSILE = 6;
+	protected static final int NOMBRE_MISSILE_TIR_INIT = 10;
+	protected static final ArrayList<Entite> MES_TIR_MISSILE = new ArrayList<>();
 
 }

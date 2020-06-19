e157a62f5e7ce82ffe0a032a21c2fad80c806f36
Antoine Auzoux <antoine.auzoux@gmail.com>
2020-06-19 14:51:02 +0200

    gestion conflits

--- src/com/cda/model/Acceuil.java
+++ src/com/cda/model/Acceuil.java
@@ -13,10 +13,10 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 class Accueil extends JPanel {
-    ImageIcon logoTeam = new ImageIcon(getClass().getResource(Constantes.LOGO));
-    Image imgLogo = logoTeam.getImage();
-<<<<<<< HEAD
-	public static String nomJoueur2 = "";
+	ImageIcon logoTeam = new ImageIcon(getClass().getResource(Constantes.LOGO));
+	Image imgLogo = logoTeam.getImage();
+
+	public static String nomJoueur2;
 	protected JTextField nomJoueur = new JTextField();
 	ImageIcon lancer = new ImageIcon("/images/bout.png");
 	private ImageIcon icoBandeFond;
@@ -26,21 +26,9 @@ class Accueil extends JPanel {
 
 	public Accueil(final MaFenetre cdaFenetre) {
 		this.setLayout(null);
-=======
-    public static String nomJoueur2;
-    protected JTextField nomJoueur = new JTextField();
-    ImageIcon lancer = new ImageIcon("/images/bout.png");
-    private ImageIcon icoBandeFond;
-    private Image imgBandeFond;
-    ImageIcon icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON));
-    Image imgMissile = icoMissile.getImage();
-    public Accueil(final MaFenetre cdaFenetre) {
-        this.setLayout(null);
->>>>>>> e334e772053700516cdecaf96b6f04837eba995e
 
 		JPanel lancerPartie = new JPanel() {
 
-<<<<<<< HEAD
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
@@ -53,57 +41,25 @@ class Accueil extends JPanel {
 		nomJoueur.setHorizontalAlignment(JTextField.CENTER);
 		nomJoueur.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
 		Font font = new Font("impact", Font.BOLD, 40);
-		nomJoueur.setBounds(200, 300, 200, 50);
+		nomJoueur.setBounds(200, 280, 200, 50);
 		nomJoueur.setFont(font);
-		lancerPartie.setBounds(200, 425, 200, 50);
+		lancerPartie.setBounds(200, 350, 200, 50);
 		this.add(lancerPartie);
 		this.add(nomJoueur);
 		setSize(cdaFenetre.getWidth(), cdaFenetre.getHeight());
 		this.icoBandeFond = new ImageIcon(getClass().getResource(Constantes.FOND));
 		this.imgBandeFond = this.icoBandeFond.getImage();
-		nomJoueur2 = this.nomJoueur.getText();
-=======
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(imgMissile, 0, 0, 200, 50, null);
-            }
-        };
-        lancerPartie.setOpaque(false);
-        JTextField nomJoueur = new JTextField();
-        nomJoueur.setOpaque(false);
-        nomJoueur.setHorizontalAlignment(JTextField.CENTER);
-        nomJoueur.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
-        Font font = new Font("impact", Font.BOLD, 40);
-        nomJoueur.setBounds(200, 280, 200, 50);
-        nomJoueur.setFont(font);
-        lancerPartie.setBounds(200, 350, 200, 50);
-        this.add(lancerPartie);
-        this.add(nomJoueur);
-        setSize(cdaFenetre.getWidth(), cdaFenetre.getHeight());
-        this.icoBandeFond = new ImageIcon(getClass().getResource(Constantes.FOND));
-        this.imgBandeFond = this.icoBandeFond.getImage();
-
->>>>>>> e334e772053700516cdecaf96b6f04837eba995e
 
 		lancerPartie.addMouseListener(new MouseAdapter() {
 
-<<<<<<< HEAD
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				super.mouseClicked(e);
-				cdaFenetre.lancerPartiePanel();
+				nomJoueur2 = nomJoueur.getText().trim();
+				if (nomJoueur2.length() != 0) {
+					cdaFenetre.lancerPartiePanel();
+				}
 			}
-=======
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                nomJoueur2 = nomJoueur.getText().trim();
-                if(nomJoueur2.length()!=0) {
-                    cdaFenetre.lancerPartiePanel();
-                }
-            }
->>>>>>> e334e772053700516cdecaf96b6f04837eba995e
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -128,7 +84,7 @@ class Accueil extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(imgBandeFond, this.getX(), this.getY(), null);
-        g.drawImage(imgLogo, 150, 500,300,333,null);
+		g.drawImage(imgLogo, 150, 500, 300, 333, null);
 
 	}
 }

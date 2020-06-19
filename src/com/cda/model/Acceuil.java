cee4aa46c46722126cbd73ef31fb1df797428603
Antoine Auzoux <antoine.auzoux@gmail.com>
2020-06-19 14:12:28 +0200

    bombe zigzag - logo

--- src/com/cda/model/Acceuil.java
+++ src/com/cda/model/Acceuil.java
@@ -1,77 +1,84 @@
 package com.cda.model;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 class Accueil extends JPanel {
-    public static String nomJoueur2 = "";
-    protected JTextField nomJoueur = new JTextField();
-    ImageIcon lancer = new ImageIcon("/images/bout.png");
-    private ImageIcon icoBandeFond;
-    private Image imgBandeFond;
-    ImageIcon icoMissile = new ImageIcon(getClass().getResource(Constantes.bouton));
-    Image imgMissile = icoMissile.getImage();
-    public Accueil(final MaFenetre cdaFenetre) {
-        this.setLayout(null);
+	public static String nomJoueur2 = "";
+	protected JTextField nomJoueur = new JTextField();
+	ImageIcon lancer = new ImageIcon("/images/bout.png");
+	private ImageIcon icoBandeFond;
+	private Image imgBandeFond;
+	ImageIcon icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON));
+	Image imgMissile = icoMissile.getImage();
 
-        JPanel lancerPartie = new JPanel() {
+	public Accueil(final MaFenetre cdaFenetre) {
+		this.setLayout(null);
 
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(imgMissile, 0, 0, 200, 50, null);
-            }
-        };
-        lancerPartie.setOpaque(false);
-        JTextField nomJoueur = new JTextField();
-        nomJoueur.setOpaque(false);
-        nomJoueur.setHorizontalAlignment(JTextField.CENTER);
-        nomJoueur.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
-        Font font = new Font("impact", Font.BOLD, 40);
-        nomJoueur.setBounds(200, 300, 200, 50);
-        nomJoueur.setFont(font);
-        lancerPartie.setBounds(200, 425, 200, 50);
-        this.add(lancerPartie);
-        this.add(nomJoueur);
-        setSize(cdaFenetre.getWidth(), cdaFenetre.getHeight());
-        this.icoBandeFond = new ImageIcon(getClass().getResource(Constantes.FOND));
-        this.imgBandeFond = this.icoBandeFond.getImage();
-        nomJoueur2 = this.nomJoueur.getText();
+		JPanel lancerPartie = new JPanel() {
 
-        lancerPartie.addMouseListener(new MouseAdapter() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(imgMissile, 0, 0, 200, 50, null);
+			}
+		};
+		lancerPartie.setOpaque(false);
+		JTextField nomJoueur = new JTextField();
+		nomJoueur.setOpaque(false);
+		nomJoueur.setHorizontalAlignment(JTextField.CENTER);
+		nomJoueur.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
+		Font font = new Font("impact", Font.BOLD, 40);
+		nomJoueur.setBounds(200, 300, 200, 50);
+		nomJoueur.setFont(font);
+		lancerPartie.setBounds(200, 425, 200, 50);
+		this.add(lancerPartie);
+		this.add(nomJoueur);
+		setSize(cdaFenetre.getWidth(), cdaFenetre.getHeight());
+		this.icoBandeFond = new ImageIcon(getClass().getResource(Constantes.FOND));
+		this.imgBandeFond = this.icoBandeFond.getImage();
+		nomJoueur2 = this.nomJoueur.getText();
 
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                cdaFenetre.lancerPartiePanel();
-            }
+		lancerPartie.addMouseListener(new MouseAdapter() {
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON_HOVER));
-                imgMissile = icoMissile.getImage();
-                lancerPartie.repaint();
-            }
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				super.mouseClicked(e);
+				cdaFenetre.lancerPartiePanel();
+			}
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                super.mouseExited(e);
-                icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON));
-                imgMissile = icoMissile.getImage();
-                lancerPartie.repaint();
-            }
-        });
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				super.mouseEntered(e);
+				icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON_HOVER));
+				imgMissile = icoMissile.getImage();
+				lancerPartie.repaint();
+			}
 
-    }
+			@Override
+			public void mouseExited(MouseEvent e) {
+				super.mouseExited(e);
+				icoMissile = new ImageIcon(getClass().getResource(Constantes.BOUTON));
+				imgMissile = icoMissile.getImage();
+				lancerPartie.repaint();
+			}
+		});
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(imgBandeFond, this.getX(), this.getY(), null);
+	}
 
-    }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(imgBandeFond, this.getX(), this.getY(), null);
+
+	}
 }
-

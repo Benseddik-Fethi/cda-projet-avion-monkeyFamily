c8e65b0b33bd64e7789bc73fa95a2d4996567c89
CDA-Benseddik-Fethi <58114380+CDA-Benseddik-Fethi@users.noreply.github.com>
2020-06-20 06:12:02 +0200

    sauvegarde score

--- src/com/cda/model/FichierSauvegarde.java
+++ src/com/cda/model/FichierSauvegarde.java
@@ -9,8 +9,6 @@ import java.util.Properties;
 
 public class FichierSauvegarde {
 
-
-
     public static void fichier()  {
         Properties vPropriete = System.getProperties();
         String vNomSession = vPropriete.getProperty("user.name");
@@ -19,22 +17,23 @@ public class FichierSauvegarde {
         LocalDateTime vDate = LocalDateTime.now();
         String vDateFormater = vDate.format(vDateFormat);
 
-        File vFichier = new File("C://temp/" + vDateFormater + ".txt");
+        File vFichier = new File("C://temp/" + "sauvegarde-jeu.txt");
         if (vFichier.exists()) {
-            System.out.println("le fichier existe ");
         } else {
-            System.out.println("le fichier n'existe pas !");
             try {
                 vFichier.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        try {
-            FileWriter vFileWriter = new FileWriter(vFichier, true);
-            vFileWriter.append(Accueil.nomJoueur2 + " " + GestionCollision.compteurGlobal +"\n"); // ma session pc ne contient que mon prenom.
+        try (FileWriter vFileWriter = new FileWriter(vFichier, true)) {
+            vFileWriter.append(vDateFormater).
+                    append(" : ")
+                    .append(Accueil.nomJoueur2)
+                    .append(" ----> ")
+                    .append(String.valueOf(GestionCollision.compteurGlobal))
+                    .append("\n");
             vFileWriter.flush();
-            vFileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
